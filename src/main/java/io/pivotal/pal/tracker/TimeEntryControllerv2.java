7afb9ec89b3082d7fd8cc2882ef1c7ff1dfe8ef6
something@example.com <bhoogter@ford.com>
2018-07-31 14:32:34 -0400

    v2.0+

--- src/main/java/io/pivotal/pal/tracker/TimeEntryControllerv2.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryControllerv2.java
@@ -7,44 +7,46 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class TimeEntryController {
-    protected TimeEntryRepository timeEntryRepository;
+public class TimeEntryControllerv2 {
+    TimeEntryRepository timeEntryRepository;
 
-    public TimeEntryController(TimeEntryRepository timeEntryRepository) {
+    public TimeEntryControllerv2(TimeEntryRepository timeEntryRepository) {
         this.timeEntryRepository = timeEntryRepository;
     }
 
-    @PostMapping("/time-entries")
+    @PostMapping(value="/time-entries", headers="X-api-version=2.0")
     public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate) {
         return new ResponseEntity(timeEntryRepository.create(timeEntryToCreate), HttpStatus.CREATED);
     }
 
-    @PutMapping(value="/time-entries/{id}")
+    @PutMapping(value="/time-entries/{id}", headers="X-api-version=2.0")
     public ResponseEntity update(@PathVariable long id, @RequestBody TimeEntry timeEntryToCreate) {
         TimeEntry res = timeEntryRepository.update(id, timeEntryToCreate);
         if (res == null) return new ResponseEntity(HttpStatus.NOT_FOUND);
         return new ResponseEntity(res, HttpStatus.OK);
     }
 
-    @DeleteMapping("/time-entries/{id}")
+    @DeleteMapping(value="/time-entries/{id}", headers="X-api-version=2.0")
     public ResponseEntity<TimeEntry> delete(@PathVariable long id) {
         timeEntryRepository.delete(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @GetMapping("/time-entries/{id}")
+    @GetMapping(value="/time-entries/{id}", headers="X-api-version=2.0")
     public ResponseEntity<TimeEntry> read(@PathVariable long id) {
         TimeEntry res = timeEntryRepository.find(id);
         if (res == null) return new ResponseEntity(HttpStatus.NOT_FOUND);
         return new ResponseEntity(res, HttpStatus.OK);
     }
 
-    @GetMapping("/time-entries")
-    public ResponseEntity<List<TimeEntry>> list() {
-        try {
-            return new ResponseEntity(timeEntryRepository.list(), HttpStatus.OK);
-        } catch(Exception e) {
-            return new ResponseEntity("", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    @GetMapping(value="/time-entries", headers="X-api-version=2.0")
+    public ResponseEntity<String> NoList() {
+        return new ResponseEntity("Some Version 2.0 content", HttpStatus.OK);
     }
+
+    @GetMapping(value="/time-entries/status", headers="X-api-version=2.0")
+    public ResponseEntity<String> status() {
+        return new ResponseEntity("Statused", HttpStatus.OK);
+    }
+
 }
