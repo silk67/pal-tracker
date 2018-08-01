ca503f2e39e83dbe4b4853ecaa518d117be1fae2
something@example.com <bhoogter@ford.com>
2018-08-01 10:50:52 -0400

    Refactor PSC

--- src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/JdbcTimeEntryRepository.java
@@ -1,6 +1,7 @@
 package io.pivotal.pal.tracker;
 
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -27,21 +28,23 @@ public class JdbcTimeEntryRepository implements TimeEntryRepository {
     public TimeEntry create(TimeEntry timeEntry) {
         KeyHolder generatedKeyHolder = new GeneratedKeyHolder();
 
-        jdbcTemplate.update(connection -> {
-                PreparedStatement statement = connection.prepareStatement(
-                        "INSERT INTO time_entries (project_id, user_id, date, hours) " +
-                        "VALUES (?, ?, ?, ?)",
-                        RETURN_GENERATED_KEYS
-                    );
+        PreparedStatementCreator psc = connection -> {
+            PreparedStatement statement = connection.prepareStatement(
+                    "INSERT INTO time_entries (project_id, user_id, date, hours) " +
+                            "VALUES (?, ?, ?, ?)",
+                    RETURN_GENERATED_KEYS
+            );
 
 
-                statement.setLong(1, timeEntry.getProjectId());
-                statement.setLong(2, timeEntry.getUserId());
-                statement.setDate(3, Date.valueOf(timeEntry.getDate()));
-                statement.setInt(4, timeEntry.getHours());
+            statement.setLong(1, timeEntry.getProjectId());
+            statement.setLong(2, timeEntry.getUserId());
+            statement.setDate(3, Date.valueOf(timeEntry.getDate()));
+            statement.setInt(4, timeEntry.getHours());
 
-                return statement;
-                }, generatedKeyHolder);
+            return statement;
+        };
+
+        jdbcTemplate.update(psc, generatedKeyHolder);
 
         long id = generatedKeyHolder.getKey().longValue();
         return find(id);
